57eaf6f76e2fd141025cf48c935df5a8e26d7d87
Noraa Stoke <noraa.july.stoke@gmail.com>
2022-11-18 13:28:21 -0500

    progress

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,13 +1,30 @@
 
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
+// middleware connections
+
+// Connect restoreUser middleware to the API router
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
 
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({requestBody: req.body});
+
+});
 
 
+module.exports = router;
 
 
 
@@ -99,7 +116,3 @@ router.use(restoreUser);
 //     return res.json(req.user)
 
 // });
-
-
-
-module.exports = router;
