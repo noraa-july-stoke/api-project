b1808b1d6eb35170731d2eeefe559fe8199908df
Noraa Stoke <noraa.july.stoke@gmail.com>
2022-11-17 22:08:21 -0500

    add users table migration and test seed

---
+++ backend/db/seeders/20221118010528-demo-user.js
@@ -0,0 +1,39 @@
+'use strict';
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Users', [{
+      email: 'noraa.july.stoke@gmail.com',
+      userName: 'ItsNoraa',
+      hashedPassword: bcrypt.hashSync('password')
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete('Users', {where: {userName: 'ItsNoraa'}})
+  }
+};
