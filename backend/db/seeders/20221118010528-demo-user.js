5fd90b4479a69f4ba99cfa5f4fc4041c00cfd000
Noraa Stoke <noraa.july.stoke@gmail.com>
2022-11-19 19:37:55 -0500

    bug fixing- seed demo user seed file

--- backend/db/seeders/20221118010528-demo-user.js
+++ backend/db/seeders/20221118010528-demo-user.js
@@ -6,34 +6,24 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-
-    await queryInterface.bulkInsert('Users', [{
+  up: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    return queryInterface.bulkInsert(options, [{
       email: 'user1@usernet.com',
       username: 'user1',
       firstName: 'User',
       lastName: 'One',
       hashedPassword: bcrypt.hashSync('password')
     },
-  {
+    {
       email: 'user2@usernet.com',
       username: 'user2',
       firstName: 'User',
       lastName: 'Two',
       hashedPassword: bcrypt.hashSync('password')
     },
-  {
+    {
       email: 'user3@usernet.com',
       username: 'user3',
       firstName: 'User',
@@ -42,14 +32,11 @@ module.exports = {
     }], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-
-    await queryInterface.bulkDelete('Users', null, { where: { firstName: 'User'}});
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['user1', 'user2', 'user3'] }
+    }, {});
   }
 };
